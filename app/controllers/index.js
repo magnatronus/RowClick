6796776492c35bbc9aa38cf82f78157d10db3dae
Steve Rogers <srogers.uk@gmail.com>
2013-12-03 09:04:42 +0000

    Updated so that row 'expand/collapse'  works by replacing row with new version and tracking expand status on row

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,20 @@
 
+function rowClicked(e){
+	if(e.row){	
+		$.list.updateRow(e.index, Alloy.createWidget('com.test.ExpandingRow', {'expanded': !e.row.expanded}).getView());
+	}	
+}
 
 
 //lets generate a few rows
 var rows = [];
 var i=0;
 for(var i=0; i<5; i++){
-	var row = Alloy.createWidget('com.test.ExpandingRow').getView();
+	var row = Alloy.createWidget('com.test.ExpandingRow', {'expanded': false}).getView();
 	rows.push(row);	
 }
-
 $.list.setData(rows);
+
 
 // now show index
 $.index.open();
--- app/widgets/com.test.ExpandingRow/controllers/widget.js
+++ app/widgets/com.test.ExpandingRow/controllers/widget.js
@@ -1,8 +1,3 @@
-var expanded = false;
-
-function reviewClick(e){
-	//alert('Click'); //uncomment me so i work on iOS
-	e.cancelBubble = true;
-	expanded = !expanded;
-	$.reviewText.height=(expanded)?Ti.UI.SIZE: '45dp';	
-}
+var args=arguments[0]||{};
+$.reviewText.height=(args.expanded)?Ti.UI.SIZE: '45dp';	
+$.reviewRow.expanded = args.expanded;
